1e029313e3277e02d305af00813499320d13907a
Yusuf Tufan <yusufftufan@gamil.com>
2025-10-18 23:52:41 +0300

    feat(project): Added Part 4 to Project 06 and updated README (Making Navigation Reusable)

---
+++ 06-custom-routing/src/hooks/use-navigation.jsx
@@ -0,0 +1,8 @@
+import { use } from "react";
+import NavigationContext from "../context/navigation";
+import { useContext } from "react";
+function useNavigation() {
+    return useContext(NavigationContext);
+}
+
+export default useNavigation;
