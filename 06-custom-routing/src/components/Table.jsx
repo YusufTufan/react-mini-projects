689180641173e5f62f531f51be0d7cb4d5f8ef0f
Yusuf Tufan <yusufftufan@gamil.com>
2025-10-22 19:14:51 +0300

    feat(project): Added Part 7 to Project 06 and updated README (Getting Clever with Data Sorting)

--- 06-custom-routing/src/components/Table.jsx
+++ 06-custom-routing/src/components/Table.jsx
@@ -1,7 +1,12 @@
+import { Fragment } from "react";
 function Table ({data, config, keyFn}) {
     const renderedHeaders = config.map((column) => {
+        if (column.header) {
+        return <Fragment key={column.label}>{column.header()}</Fragment>
+        }
+
         return <th key={column.label}>{column.label}</th>;
-    })
+    });
 
     const renderedRows = data.map((rowData) => {
         const renderedCells = config.map((column) => {
@@ -16,7 +21,7 @@ function Table ({data, config, keyFn}) {
                 {renderedCells}
             </tr>
         );
-    })
+    });
 
     return (
     <table className="table-auto border-spacing-2">
